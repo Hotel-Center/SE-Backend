b559a5582bcadb60844ebf86b193cf3a3066d70e
mohammad amin geramimehr <59396870+MohammadAmin-2001@users.noreply.github.com>
2023-07-06 10:55:46 +0330

    fix : permissions in ticket view
     change role permission  for access to apis

--- HotelCenter/ticket/views.py
+++ HotelCenter/ticket/views.py
@@ -10,28 +10,30 @@ from rest_framework import status
 from .serializers import RequestFormSerializer, TicketFormSerializer, AdminTicketSerializer
 from .models import RequestForm, TicketForm
 # Create your views here.
-from HotelCenter.permissions import IsCustomer, IsManager
+from HotelCenter.permissions import IsCustomer, IsManager, IsAdmin
 
 
 class TypeRequestsList(ListCreateAPIView):  # get list type request and post it
+    permission_class= [IsAdmin]
     serializer_class = RequestFormSerializer
     queryset = RequestForm.objects.all()
 
 
 class MyTicketList(ListCreateAPIView):  # get list ticket and post it
-    permission_classes = [IsAuthenticated, IsCustomer]
+    permission_classes = [IsAuthenticated]
 
     serializer_class = TicketFormSerializer
     queryset = TicketForm.objects.all()
 
 
 class ResponseAdminAPIs(RetrieveUpdateAPIView):  # get one ticket and update it
-    queryset = TicketForm.objects.all()
+    permission_classes = [IsAuthenticated, IsAdmin]
+    queryset = TicketForm.objects.all,()
     serializer_class = AdminTicketSerializer
 
 
 class ShowAdminTicketList(ListAPIView):
-    permission_classes = [IsAuthenticated, IsManager]
+    permission_classes = [IsAuthenticated, IsAdmin]
 
     queryset = TicketForm.objects.all()
     serializer_class = AdminTicketSerializer
